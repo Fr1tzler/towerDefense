5d573eb47d246847fa7a8e540b21d9d26cdfb5f1
Fr1tzler <fritsler.ve@phystech.edu>
2021-04-09 01:20:48 +0500

    v0.01b - add font, add tile styles, add a few script strings

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", main);
 
+let currentGameStage;
+const gameStages = ['authorizationScreen', 'newGameScreen', 'levelSelectScreen', 'gameScreen', 'gameEndScreen'];
+
 function main() {
     return;
 }
+
+let tempMap = [
+    [0, 1, 4, 1, 0, 0],
+    [0, 1, 2, 1, 0, 0],
+    [0, 1, 2, 1, 1, 0],
+    [0, 1, 2, 2, 1, 0],
+    [0, 1, 1, 2, 1, 0],
+    [0, 0, 1, 3, 1, 0],
+]
+
+/*
+    0 - emptyTile,
+    1 - towerPlatformTile,
+    2 - roadTile,
+    3 - spawnTile,
+    4 - baseTile
+*/

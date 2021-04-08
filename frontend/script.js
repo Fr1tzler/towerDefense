88b9f821bbea833d6059beb710a4e3d96b4a888e
Fr1tzler <fritsler.ve@phystech.edu>
2021-04-09 04:06:08 +0500

    v0.02 - add simple map generation

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", init);
 
-let currentGameStage;
+const fps = 50;
 const gameStages = ['authorizationScreen', 'newGameScreen', 'levelSelectScreen', 'gameScreen', 'gameEndScreen'];
+let currentGameStage = gameStages[0];
 
-function main() {
-    return;
-}
 
 let tempMap = [
     [0, 1, 4, 1, 0, 0],
@@ -14,7 +12,7 @@ let tempMap = [
     [0, 1, 2, 2, 1, 0],
     [0, 1, 1, 2, 1, 0],
     [0, 0, 1, 3, 1, 0],
-]
+];
 
 /*
     0 - emptyTile,
@@ -23,3 +21,45 @@ let tempMap = [
     3 - spawnTile,
     4 - baseTile
 */
+
+function init() {
+    renderMap(tempMap);
+    mainloop();
+    return;
+}
+
+function mainloop() {
+    // DO SOMETHING
+    // let timer = setTimeout(mainloop, 1000 / fps);
+    return;
+}
+
+function renderMap(map) {
+    for (let i = 0; i < map.length; i++) {
+        let tileRow = document.createElement('div');
+        tileRow.className = "tileRow";
+        for (let j = 0; j < map[i].length; j++) {
+            let tile = document.createElement('div');
+            tile.className = 'tile';
+            switch (map[i][j]) {
+                case 0:
+                    tile.className += " emptyTile";
+                    break;
+                case 1:
+                    tile.className += " towerPlatformTile";
+                    break;
+                case 2:
+                    tile.className += " roadTile";
+                    break;
+                case 3:
+                    tile.className += " spawnTile";
+                    break;
+                case 4:
+                    tile.className += " baseTile";
+            }
+            tile.id = `tile_${i}_${j}`;
+            tileRow.appendChild(tile);
+        }
+        document.getElementById('tileContainer').appendChild(tileRow);
+    }
+}

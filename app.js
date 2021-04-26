b002a2de441e4f78dbbe1b19156f36f6855e32a6
Fr1tzler <fritsler.ve@phystech.edu>
2021-04-26 13:21:27 +0500

    backend v0.02 - web server is now awaiting database to be done

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
 const express = require("express");
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
     response.sendFile(`${__dirname}/frontend/index.html`);
 });
 
+app.get('/get_map', (request, response) => {
+    let mapPage = request.query.mapPage;
+    // TODO: make request to database, extract maps with pageId == mapPage
+    response.send({ 'do you want data' : 'i will not give it' });
+});
+
+
+app.post('/handle', (request, response) => {
+    let username = request.body.username;
+    let score = request.body.score;
+    let mapId = request.body.mapId;
+    let timePlayed = request.body.timePlayed;
+    // TODO: Make one more request to database
+    response.send({
+        '1' : 'Lupa',
+        '2' : 'Lupa',
+        '3' : 'Lupa',
+        '4' : 'Lupa',
+        '5' : 'Lupa',
+        '6' : 'Lupa',
+        '7' : 'Lupa',
+        '8' : 'Lupa',
+        '9' : 'Lupa',
+        '10e6' : username
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 });
-
-// TODO:
-// 1) Подключить БД и настроить работу с запросами к базе
-//      a) Получать GET запросы и возвращать карты. 
-//      b) Получать POST запросы (в них - статы игры) и возвращать позицию игрока в лидерборде

7116121c1fe3edaeaeca05a0d8d3cd5238569876
Fr1tzler <fritsler.ve@phystech.edu>
2021-04-29 18:46:09 +0500

    frontend v0.06 - add game screen and basic tile generation

--- frontend/script.js
+++ frontend/script.js
@@ -1,31 +1,55 @@
-const serverLocation = "localhost";
-
 let mapPage = 0;
-let username = "Victor";
+let username = 'username';
 let score = 0;
 let mapId = 0;
+
+const gameStages = [
+    'pregame',
+    'game',
+    'aftergame'
+]
+let currentGameStage = 0;
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log("Иди своей дорогой, сталкер.");
-    document.getElementById('btn1').onclick = requestMapPage;
-    document.getElementById('btn2').onclick = requestGameStats;
+    console.log('Иди своей дорогой, сталкер.');
+    let tileScreen = document.getElementById('tileScreen');
+    for (let i = 0; i < 10; i++) {
+        let row = document.createElement('div');
+        row.className = 'tileRow';
+        tileScreen.appendChild(row);
+        for (let j = 0; j < 10; j++) {
+            let tile = document.createElement('div');
+            tile.className = 'tile';
+            row.appendChild(tile);
+        }
+    }
+    screenChangingLoop();
+}
+
+function screenChangingLoop() {
+    document.getElementById(gameStages[currentGameStage]).style.opacity = "0";
+    document.getElementById(gameStages[currentGameStage]).style.visibility = "hidden";
+    currentGameStage = (currentGameStage + 1) % 3;
+    document.getElementById(gameStages[currentGameStage]).style.opacity = "1";
+    document.getElementById(gameStages[currentGameStage]).style.visibility = "visible";
+    setTimeout(screenChangingLoop, 5000);
 }
 
 const colors = [
-    "#FF3C00", // orangered
-    "#F79A00", // orange
-    "#F2C500", // orangeyellow
-    "#F0FF00", // yellow
-    "#BAFF00", // yellowgreen
-    "#4CBE2C", // green
-    "#2F77CF", // lightblue
-    "#5700FF", // blue
-    "#53009F", // violetblue
-    "#9000AD", // violet
-    "#A4003D", // cherry
-    "#FF0000", // red
+    '#FF3C00', // orangered
+    '#F79A00', // orange
+    '#F2C500', // orangeyellow
+    '#F0FF00', // yellow
+    '#BAFF00', // yellowgreen
+    '#4CBE2C', // green
+    '#2F77CF', // lightblue
+    '#5700FF', // blue
+    '#53009F', // violetblue
+    '#9000AD', // violet
+    '#A4003D', // cherry
+    '#FF0000', // red
 ];
 
 function countNextLevel(absorberTowerLevel, eatedTowerLevell) {
@@ -68,7 +92,7 @@ function requestGameStats() {
         mapId: mapId
     };
     fetch('/send_game_stats', {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify(requestData),
     })
     .then(response => {return response.json();})
